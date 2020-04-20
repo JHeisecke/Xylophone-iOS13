e5b2f184b358ba88a2d164904aa4be84f1bdaa97
Javier Heisekce <javierheiseckee@gmail.com>
2020-04-20 11:46:43 -0400

    Ajusta sonidos por nota

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -18,11 +18,15 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound()
+        if let text = sender.currentTitle {
+            playSound(note: text)
+        }
     }
     
-    func playSound() {
-        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+    func playSound(note : String) {
+
+        let url = Bundle.main.url(forResource: note, withExtension: "wav")
+
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
                 
